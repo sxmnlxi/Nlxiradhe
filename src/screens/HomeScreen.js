4b6a9a42d27d8fdbf758486e654775052cfbf825
sxmnlxi <saksham8d@gmail.com>
2026-09-12 05:12:04 +0530

    Create HomeScreen.js

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import PlaceholderScreen from './PlaceholderScreen';
+
+export default function HomeScreen() {
+  return <PlaceholderScreen title="Home" icon="home-outline" />;
+}
