f44bcef12032794217901d27abeda2d0a7b2ae94
sxmnlxi <saksham8d@gmail.com>
2026-09-12 05:14:35 +0530

    Create ReferScreen.js

---
+++ src/screens/ReferScreen.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import PlaceholderScreen from './PlaceholderScreen';
+
+export default function ReferScreen() {
+  return <PlaceholderScreen title="Refer & Earn" icon="gift-outline" />;
+}
