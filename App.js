20c38f7b1c992588e29ae1408af5f8ca46d76cb8
sxmnlxi <saksham8d@gmail.com>
2026-09-12 10:53:09 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
+import LoginScreen from './src/screens/LoginScreen';
 
 export default function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <SafeAreaProvider>
       <StatusBar style="dark" />
-      <AppNavigator />
+      {user ? (
+        <AppNavigator />
+      ) : (
+        <LoginScreen onLoginSuccess={(userInfo) => setUser(userInfo)} />
+      )}
     </SafeAreaProvider>
   );
 }
