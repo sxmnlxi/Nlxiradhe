fb35f6bafd75ffae333aacc6326bcc95b6700bc6
sxmnlxi <saksham8d@gmail.com>
2026-09-14 14:32:24 +0530

    Update AppNavigator.js

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
 
-// Import your screens
 import HomeScreen from '../screens/HomeScreen';
 import OffersScreen from '../screens/OffersScreen';
 import ReferScreen from '../screens/ReferScreen';
